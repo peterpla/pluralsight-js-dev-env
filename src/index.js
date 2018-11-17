9e916d9293daaeb25e3961274e7c8d16b880497b
Peter Plamondon <peter@plamondon.net>
2018-11-17 14:04:43 -0800

    add delete support, remove stray quote in table creation code

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import {getUsers} from './api/userApi';
+import {getUsers, deleteUser} from './api/userApi';
 import './index.css';
 
 // Populate table of users via API call
@@ -7,7 +7,7 @@ getUsers().then(result => {
 
   result.forEach(user => {
     usersBody+= `<tr>
-    <td><a href="#" data-id=${user.id}" class="deleteUser">Delete</a></td>
+    <td><a href="#" data-id=${user.id} class="deleteUser">Delete</a></td>
     <td>${user.id}</td>
     <td>${user.firstName}</td>
     <td>${user.lastName}</td>
@@ -16,4 +16,19 @@ getUsers().then(result => {
   });
 
   global.document.getElementById('users').innerHTML = usersBody;
+
+  const deleteLinks = global.document.getElementsByClassName('deleteUser');
+
+  // Must use array.from to create a real array from a DOM collection
+  // getElementsByClassName only returns an "array like" object
+  Array.from(deleteLinks, link => {
+    link.onclick = function(event) {
+      const element = event.target;
+      event.preventDefault();
+      deleteUser(element.attributes["data-id"].value);
+      const row = element.parentNode.parentNode;
+      row.parentNode.removeChild(row);
+    };
+  });
+
 });
